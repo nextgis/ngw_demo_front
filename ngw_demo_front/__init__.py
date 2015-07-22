5b08744b6a1678b4c8663be1a5a1e3bf3da05712
karavanjo <jownkiv@gmail.com>
2015-07-22 19:42:06 +0300

    #4 db configuration was temporarily turned off

--- ngw_demo_front/__init__.py
+++ ngw_demo_front/__init__.py
@@ -7,9 +7,9 @@ from ngw_demo_front.model import Base, DBSession
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.bind = engine
+    # engine = engine_from_config(settings, 'sqlalchemy.')
+    # DBSession.configure(bind=engine)
+    # Base.metadata.bind = engine
     config = Configurator(settings=settings)
     config.include('pyramid_mako')
     config.add_static_view('static', 'static', cache_max_age=3600)
